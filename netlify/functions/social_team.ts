675f149a47500ceadb2d981265b408a9f5b3d9f5
cool-appz <137733275+cool-appz@users.noreply.github.com>
2023-08-09 02:33:52 -0400

    logs

--- netlify/functions/social_team.ts
+++ netlify/functions/social_team.ts
@@ -1,9 +1,10 @@
 import type { Handler, HandlerEvent, HandlerContext } from "@netlify/functions";
 
 const handler: Handler = async (event: HandlerEvent, context: HandlerContext) => {
+  console.log(event)
   return {
     statusCode: 200,
-    body: "<@U04L9MD7AMS> Hello World"
+    body: "<@U04L9MD7AMS> Hello World" + event
   };
 };
 
